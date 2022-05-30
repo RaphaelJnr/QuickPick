b75c4725fc6fb85ff5f515ef8a6eb2f05b28e414
RaphaelJnr <raphealosroagbo70@gmail.com>
2022-05-30 03:05:11 +0000

    Fix With Database Connection

--- backend/dbRelated/dbConnector.php
+++ backend/dbRelated/dbConnector.php
@@ -3,23 +3,23 @@
 
     //--IN USING A LOCALHOST
 
-    $serve      = "localhost:3306";
-    $user       = "root";
-    $pin        = "";
-    $database   = "QUICKPICK";
+    // $serve      = "localhost:3306";
+    // $user       = "root";
+    // $pin        = "";
+    // $database   = "QUICKPICK";
 
-    $connect = mysqli_connect($serve, $user, $pin, $database);
+    // $connect = mysqli_connect($serve, $user, $pin, $database);
 
     
     //USING HEROKU
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"],1);
-    // $active_group = 'default';
-    // $query_builder = TRUE;
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
 
     // Connect to DB
-    // $connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
